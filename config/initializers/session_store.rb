ef2ace8abfa98a0d970bf6ddb094afa74f4d27ce
unknown <jqc4315@RTPWL10D00219.wmservice.corpnet1.com>
2012-10-15 22:08:12 -0400

    found problem in file when deploying to heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TkdTracker::Application.config.session_store :cookie_store, key: '_tkd_tracker_session'
+TkdTracker::Application.config.session_store :cookie_store, :key => '_tkd_tracker_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
